0245cd8ff961080704c2020d8c6c443ac7bb7ad2
ovictormiranda <carreira.victormiranda@gmail.com>
2021-10-18 22:19:43 -0300

    Parte 1 - Concluída

--- src/index.js
+++ src/index.js
@@ -1,33 +1,89 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "Usuário não encontrado :("});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "Já existe um usuário com este username, por favor escolha outro para prosseguir." })
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  console.log(users)
+  return response.status(201).send();
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+  console.log(user);
+  
+  const userTodos = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  }
+  console.log(userTodos); 
+
+  user.todos.push(userTodos);
+
+  return response.status(201).send();
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+  console.log("update down here");
+  
+  user.todos = { 
+    title, 
+    deadline,
+  }
+
+  console.log(user);
+  console.log("between");
+  
+  return response.status(201).send({ title, deadline });
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
